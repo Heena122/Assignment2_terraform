1c63aa71eedbcdbc52c94b5929b7eca5b04bbb3b
Heena122 <126157263+Heena122@users.noreply.github.com>
2024-02-12 18:43:18 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,3 +2,7 @@ variable "bucket_list"{
     type = list(string)
     default = [ "12prac34","34prac56","22prac47" ]
 }
+variable "bucket_list2"{
+    type = list(string)
+    default = [ "pra11","pra12","pra13" ]
+}
