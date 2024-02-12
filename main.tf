18c224e24932f5e18d4ec02caccbfa7db2e2e27d
Heena122 <126157263+Heena122@users.noreply.github.com>
2024-02-12 18:42:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "exampless" {
   count = length((var.bucket_list))
   bucket = var.bucket_list[count.index]
- 
-   # for_each = toset(var.bucket_list)
-   # bucket = each.key # each-- for index
+
+
+}
+
+resource "aws_s3_bucket" "exampless2" {
+    for_each = toset(var.bucket_list2)
+    bucket = each.key # each-- for index
+
 
 }
